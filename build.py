e9af7deaa53619eecb5c2a227dc911aac3e8931e
mkonnerth <matkonnerth@gmail.com>
2020-06-20 09:13:25 +0200

    add upload

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ from cpt.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.build_policy = "missing"
+    builder.upload = ("https://api.bintray.com/conan/matkonnerth/cpprepo", False, "upload_repo")
     builder.add(settings={"arch": "x86_64", "build_type": "Debug"},
                 options={}, env_vars={}, build_requires={})
     builder.add(settings={"arch": "x86_64", "build_type": "RelWithDebInfo"},
