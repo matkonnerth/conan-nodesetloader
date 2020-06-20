2e4b4361785e42c3ec7cafd82957b9df16ff13ff
mkonnerth <matkonnerth@gmail.com>
2020-06-20 08:29:02 +0200

    add build missing to policy

--- build.py
+++ build.py
@@ -3,6 +3,7 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
+    builder.build_policy = "missing"
     builder.add(settings={"arch": "x86_64", "build_type": "Debug"},
                 options={}, env_vars={}, build_requires={})
     builder.add(settings={"arch": "x86_64", "build_type": "RelWithDebInfo"},
